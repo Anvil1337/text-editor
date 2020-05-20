ad7d4861331733d161d5cc2aad3c3fe2fb4c098f
Roman Burakov <buracov.r@gmail.com>
2020-05-21 00:24:34 +0300

    Added Save and Open features

--- Editor.py
+++ Editor.py
@@ -1,5 +1,5 @@
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from GUI import Ui_MainWindow
 import sys
 
@@ -12,14 +12,27 @@ class Editor(QMainWindow):
 
         self.ui.apply_button.clicked.connect(self.apply_changes)
 
+        self.ui.menu_save.triggered.connect(self.save)
+
+        self.ui.menu_open.triggered.connect(self.open)
+
     def apply_changes(self):
-        self.ui.main_text.setFont(QFont(self.ui.select_font_comboBox.currentText(), int(self.ui.font_size_spinBox.value())))
+        self.ui.main_text.setFont(QFont(self.ui.select_font_comboBox.currentText(),
+                                        int(self.ui.font_size_spinBox.value())))
 
     def save(self):
-        pass
+        file_name = QFileDialog.getSaveFileName(self, 'Save File')[0]
+        if file_name != '':
+            with open(file_name, 'a') as f:
+                f.seek(0)
+                f.truncate()
+                f.write(self.ui.main_text.toPlainText())
 
     def open(self):
-        pass
+        file_name = QFileDialog.getOpenFileName(self, 'Open File')[0]
+        if file_name != '':
+            with open(file_name, 'r') as file:
+                self.ui.main_text.setText(file.read())
 
 
 if __name__ == "__main__":
