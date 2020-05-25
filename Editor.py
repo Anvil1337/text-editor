a8a05c7bbd8af2dc4801c23bd9699ddf28085f27
Roman Burakov <buracov.r@gmail.com>
2020-05-25 09:34:24 +0300

    Fixed save function and font change updates.

--- Editor.py
+++ Editor.py
@@ -1,3 +1,5 @@
+import os
+
 from PyQt5.QtCore import Qt, QFileInfo
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
@@ -14,15 +16,13 @@ class Editor(QMainWindow):
         self.ui.main_text.setText('Hello!')
 
         self.ui.menu_save.triggered.connect(self.save)
-
         self.ui.menu_open.triggered.connect(self.open)
-
         self.ui.menu_print.triggered.connect(self.print_file)
-
         self.ui.menu_print_preview.triggered.connect(self.print_preview)
-
         self.ui.menu_export_pdf.triggered.connect(self.export_pdf)
-        
+
+        self.text_weight = lambda: QFont.Bold if self.ui.bold_checkBox.isChecked() else 0
+
         self.ui.left_alignment.triggered.connect(
             lambda: self.ui.main_text.setAlignment(Qt.AlignLeft))
 
@@ -36,7 +36,7 @@ class Editor(QMainWindow):
             lambda: self.ui.main_text.setAlignment(Qt.AlignJustify))
 
         self.ui.bold_checkBox.toggled.connect(
-            lambda: self.ui.main_text.setFontWeight(QFont.Bold if self.ui.bold_checkBox.isChecked() else 0))
+            lambda: self.ui.main_text.setFontWeight(self.text_weight()))
 
         self.ui.italic_checkBox.toggled.connect(
             lambda: self.ui.main_text.setFontItalic(self.ui.italic_checkBox.isChecked()))
@@ -47,17 +47,23 @@ class Editor(QMainWindow):
         self.ui.font_size_spinBox.valueChanged.connect(
             lambda: self.ui.main_text.setFontPointSize(self.ui.font_size_spinBox.value()))
 
-        self.ui.select_font_comboBox.currentTextChanged.connect(
-            lambda: self.ui.main_text.setCurrentFont(QFont(self.ui.select_font_comboBox.currentText(),
-                                                           self.ui.font_size_spinBox.value())))
+        self.ui.select_font_comboBox.currentTextChanged.connect(self.change_font)
+
+    def change_font(self):
+        self.ui.main_text.setCurrentFont(QFont(self.ui.select_font_comboBox.currentText(),
+                                               self.ui.font_size_spinBox.value(),
+                                               self.text_weight(),
+                                               self.ui.italic_checkBox.isChecked()))
+
+        self.ui.main_text.setFontUnderline(self.ui.underline_checkBox.isChecked())
 
     def save(self):
-        file_name = QFileDialog.getSaveFileName(self, 'Save File')[0]
-        if file_name != '':
-            with open(file_name, 'a') as f:
-                f.seek(0)
-                f.truncate()
-                f.write(self.ui.main_text.toPlainText())
+        file_name, _ = QFileDialog.getSaveFileName(self, 'Save file', '', 'HTML (*.html, *htm); Text documents (*.txt)')
+        if file_name:
+            text = self.ui.main_text.toHtml() if os.path.splitext(file_name)[1] in ['.html', '.htm'] \
+                else self.ui.main_text.toPlainText()
+            with open(file_name, 'w') as f:
+                f.write(text)
 
     def open(self):
         file_name = QFileDialog.getOpenFileName(self, 'Open File')[0]
@@ -74,14 +80,13 @@ class Editor(QMainWindow):
 
     def print_preview(self):
         printer = QPrinter(QPrinter.HighResolution)
-        print(123)
         preview_dialog = QPrintPreviewDialog(printer, self)
         preview_dialog.paintRequested.connect(
             lambda: self.ui.main_text.print_(printer))
         preview_dialog.exec_()
 
     def export_pdf(self):
-        fn, _ = QFileDialog.getSaveFileName(self, 'Export to PDF', None, 'PDF files (.pdf) ;; All Files')
+        fn, _ = QFileDialog.getSaveFileName(self, 'Export to PDF', '', 'PDF files (.pdf) ;; All Files')
         if fn != '':
             if QFileInfo(fn).suffix() == '':
                 fn += '.pdf'
